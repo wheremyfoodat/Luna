72fc1b03ff08385b96d94a7feb9b67aae7c9b186
kappamalone <uzman.zawahir1@gmail.com>
2021-08-05 08:16:43 +0530

    align stack before calling function

--- examples/HelloWorld.cpp
+++ examples/HelloWorld.cpp
@@ -7,9 +7,16 @@ int main() {
     const char* str = "Hello World!\n";
     Generator code;
 
+    auto stackOffset = 8;  // The offset needed to align the stack.
+#ifdef _WIN32
+        stackOffset += 32; // Windows needs an extra 32 bytes because of the "shadow" stack space
+#endif
+
     code.mov (rcx, (u64) str); // pointer to the string in rcx
     code.mov (rax, (u64) &printf); // pointer to printf in rax
+    code.sub(rsp, stackOffset); //align stack to 16 byte boundary
     code.call (rax); // call printf
+    code.add(rsp, stackOffset); //return stack to original position
     code.ret();      // return
 
     auto foo = (FunctionPointer) code.data(); // function pointer to the generated function
